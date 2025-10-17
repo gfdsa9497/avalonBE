197573c3dfa7e7904bb750e93963062d49ce3e55
Jinwook Lee <gfdsa9497@naver.com>
2025-10-17 17:45:42 +0900

    Initiallize avalon project

---
+++ src/main/java/com/leejinwook/avalonBE/AvalonBeApplication.java
@@ -0,0 +1,13 @@
+package com.leejinwook.avalonBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalonBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leejinwook/avalonBE/AvalonBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leejinwook.avalonBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalonBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
